3018390bd89c558d25eb8702bcc7c27797294120
punav <aparna.tengse@gmail.com>
2017-02-02 21:51:52 -0800

    changed something

---
+++ main/com/java/sampleCode.java
@@ -0,0 +1,12 @@
+public class sampleCode{
+
+    sampleCode(String val){
+        String a = val;
+        System.out.print("successful Jenkin integration yaay”);
+    }
+
+    public static void main(Strings[] args){
+        sampleCode("test string");
+    }
+
+}
